be5e69faadeef8c4970ec043005612fc3313c9e7
t0x0t <govkotleta@yandex.ru>
2019-07-05 04:24:40 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -8,9 +8,11 @@ class ImageMessage (
     chat: Chat,
     isIncoming: Boolean = false,
     date: Date = Date(),
-    var image:String
+    var image:String,
+    val type:String="изображение"
 ) : BaseMessage(id, from, chat, isIncoming, date) {
 
-    override  fun formatMessage() : String = "id:$id ${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение $image $date"
+    override  fun formatMessage() : String = "$id ${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение $type"
+    //override  fun formatMessage() : String = "id:$id ${from?.firstName} ${if(isIncoming) "получил" else "отправил"} изображение $image $date"
 
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -8,8 +8,10 @@ class TextMessage(
     chat: Chat,
     isIncoming: Boolean = false,
     date:Date = Date(),
-    var text:String
+    var text:String,
+    val type:String="сообщение"
 ) : BaseMessage(id, from, chat, isIncoming, date) {
 
-    override  fun formatMessage() : String = "id:$id ${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение $text $date"
+    override  fun formatMessage() : String = "$id ${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение $type"
+    //override  fun formatMessage() : String = "$id ${from?.firstName} $isIncoming $text $date"
 }

3bfd5da58c55837c0d1f0311fa2a4f0b1626fb8d
t0x0t <govkotleta@yandex.ru>
2019-07-13 00:21:25 +0300

    hometask_3 add Activity.hideKeyboard() add send on click DONE

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,30 +1,35 @@
 package ru.skillbranch.devintensive
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
+import  android.view.inputmethod.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var benderImage: ImageView
     lateinit var textTXT: TextView
     lateinit var messageEt: EditText
-    lateinit var  sendBtn: ImageView
+    lateinit var sendBtn: ImageView
     lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        click_on_done()
         benderImage = iv_bender
         textTXT = tv_text
         messageEt = et_message
@@ -35,9 +40,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
         Log.d("M_MainActivity", "onCreate $status $question")
         val (r, g, b) = benderObj.status.color
-        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
-        textTXT.text= benderObj.askQuestion()
+        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+        textTXT.text = benderObj.askQuestion()
         sendBtn.setOnClickListener(this)
+
+
     }
 
 
@@ -80,15 +87,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    override fun onClick(v: View?)
-    {
-        if (v?.id==R.id.iv_send)
-        {
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
             val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
             messageEt.setText("")
             val (r, g, b) = color
-            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
             textTXT.text = phrase
+        }
+    }
+
+    fun click_on_done()
+    {
+        et_message.setOnEditorActionListener() { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+                messageEt.setText("")
+                val (r, g, b) = color
+                benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+                textTXT.text = phrase
+                hideKeyboard()
+                Log.d("M_MainActivity", "Клик по DONE")
+                true
+            } else {
+                false
+            }
         }
     }
 }
